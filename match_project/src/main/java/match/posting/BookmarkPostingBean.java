1f74c4763f8c5770682731a4a8e920a4bcf8c996
HWJ <hwj5614@gmail.com>
2024-04-11 18:19:25 +0900

    gd2

---
+++ match_project/src/main/java/match/posting/BookmarkPostingBean.java
@@ -0,0 +1,19 @@
+package match.posting;
+
+public class BookmarkPostingBean {
+	private String user_id;
+	private int posting_idx;
+	
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public int getPosting_idx() {
+		return posting_idx;
+	}
+	public void setPosting_idx(int posting_idx) {
+		this.posting_idx = posting_idx;
+	}
+}
